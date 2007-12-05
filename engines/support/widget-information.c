f5b93ce411db804f84624a41b14a5dae5d6c4c8b
Benjamin Berg <benjamin@sipsolutions.net>
2007-12-05 12:53:03 +0000

    Removed some dead code. (Bug #501611, reported by coverity)
    2007-12-05  Benjamin Berg  <benjamin@sipsolutions.net>
    
    	* engines/support/widget-information.c:
    	(ge_combo_box_is_using_list):
    	Removed some dead code. (Bug #501611, reported by coverity)
    
    svn path=/trunk/; revision=989

--- engines/support/widget-information.c
+++ engines/support/widget-information.c
@@ -207,14 +207,7 @@ ge_combo_box_is_using_list (GtkWidget * widget)
   gboolean result = FALSE;
  
   if (GE_IS_COMBO_BOX (widget))
-    {
-      gboolean *tmp = NULL;
- 
-      gtk_widget_style_get (widget, "appears-as-list", &result, NULL);
-
-      if (tmp)
-	result = *tmp;
-    }
+    gtk_widget_style_get (widget, "appears-as-list", &result, NULL);
  
   return result;
 }
