30f6df9f2017f7c4b7b8c1f2577f8b65ead2d866
Colin Walters <walters@verbum.org>
2012-10-30 10:35:06 -0400

    autogen: use gnome-common
    The hardcoded list of automake versions we have in here is lame, and
    actually blew up lately in gnome-ostree since I'm trying to rebase to
    Poky "Danny" which only has automake-1.12.

--- autogen.sh
+++ autogen.sh
@@ -3,79 +3,18 @@
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
-DIE=0
 
-THEDIR=`pwd`
-cd $srcdir
+PKG_NAME="gtk-engines"
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf installed to compile GTK+."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at http://ftp.gnu.org/gnu/autoconf/"
-	DIE=1
+(test -f $srcdir/Makefile.am) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
 }
 
-if automake-1.11 --version < /dev/null > /dev/null 2>&1 ; then
-    AUTOMAKE=automake-1.11
-    ACLOCAL=aclocal-1.11
-elif automake-1.10 --version < /dev/null > /dev/null 2>&1 ; then
-    AUTOMAKE=automake-1.10
-    ACLOCAL=aclocal-1.10
-elif automake-1.9 --version < /dev/null > /dev/null 2>&1 ; then
-    AUTOMAKE=automake-1.9
-    ACLOCAL=aclocal-1.9
-elif automake-1.8 --version < /dev/null > /dev/null 2>&1 ; then
-    AUTOMAKE=automake-1.8
-    ACLOCAL=aclocal-1.8
-elif automake-1.7 --version < /dev/null > /dev/null 2>&1 ; then
-    AUTOMAKE=automake-1.7
-    ACLOCAL=aclocal-1.7
-else
-        echo
-        echo "You must have automake 1.7.x installed to compile $PROJECT."
-        echo "Install the appropriate package for your distribution,"
-        echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
-        DIE=1
-fi
-
-(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have libtool installed to compile GTK+."
-	echo "Get http://ftp.gnu.org/gnu/libtool/libtool-1.5.10.tar.gz"
-	echo "(or a newer version if it is available)"
-	DIE=1
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common"
+    exit 1
 }
 
-(intltoolize --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have intltool installed to compile gtk-engines."
-	DIE=1
-}
-                        
-
-if test "$DIE" -eq 1; then
-	exit 1
-fi
-
-if test -z "$*"; then
-	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-fi
-
-libtoolize --force --copy
-intltoolize --force --copy --automake
-
-
-$ACLOCAL $ACLOCAL_FLAGS
-autoconf
-autoheader
-$AUTOMAKE --add-missing
-cd $THEDIR
-
-if test -z "$NOCONFIGURE"; then
-    $srcdir/configure --enable-maintainer-mode "$@"
-
-    echo 
-    echo "Now type 'make' to compile themes"
-fi
+. gnome-autogen.sh --enable-gtk-doc $*
